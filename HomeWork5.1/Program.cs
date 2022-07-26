9f1603e1b369cb3205f0e31d39502f77ed01a3c9
Hz31415 <56264718+Hz31415@users.noreply.github.com>
2022-07-26 10:45:35 +0400

    Add files via upload

---
+++ HomeWork5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using HomeWork5._1;
+
+
+Console.WriteLine("Do You want to test first or second task (Type 1/2)");
+
+string taskNum = Console.ReadLine();
+
+if (taskNum == "1")
+{
+    Console.WriteLine("Please input tour sentence");
+    string sentence = Console.ReadLine(); //можно было и в нижюю строку вписать - SentenceProcessing.WordsArrOut(SentenceProcessing.Splitter(Console.ReadLine()));
+
+    SentenceProcessing.WordsArrOut(SentenceProcessing.Splitter(sentence));
+}
+else if (taskNum == "2")
+{
+    Console.WriteLine("Please input tour sentence");
+    string sentence = Console.ReadLine();
+    SentenceProcessing.WordsArrOut(SentenceProcessing.ReversWords(sentence));
+}
+
---
+++ HomeWork5.1/SentenceProcessing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._1
+{
+    internal class SentenceProcessing
+    {
+        public static string[] Splitter(string inputStr)
+        {
+            string[] words = inputStr.Split(' ');
+            return words;
+        }
+
+        public static string[] ReversWords(string inputPhrase)
+        {
+            return (string[])Splitter(inputPhrase).Reverse();
+        }
+
+        public static void WordsArrOut(string[] strArray)
+        { 
+            foreach(string str in strArray)
+                Console.WriteLine(str);
+        }
+
+
+    }
+}
